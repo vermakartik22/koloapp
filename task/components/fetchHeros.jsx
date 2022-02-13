30da58f40b47ae99456e8f6ebc6017bcb2eb9511
kevy <vermakartk22@gmail.com>
2022-02-13 22:58:18 +0530

    fixes

--- task/components/fetchHeros.jsx
+++ task/components/fetchHeros.jsx
@@ -9,7 +9,6 @@ const FetchHeros = function(){
     useEffect(()=>{
         getMarvelCharactars().then((response) => {
             if (response && response.status === 200) {
-                console.log(response.data.data.results);
                 setHeros(response && response.data.data.results);
                 setFullArr(response && response.data.data.results);
             }
@@ -17,33 +16,23 @@ const FetchHeros = function(){
     },[]);
 
     const searchHero = (value) =>{
-        console.log(value.target.value);
         let val = value.target.value;
-        // let pos = heros.filter(h => h.name === val);
-        // console.log(pos);
-        
         const regex = new RegExp(val);
-        //const check = new Promise(resolve => resolve(heros.filter(h => h.name.match(regex))))
         let pos = heros.filter(h => h.name.match(regex));
-        console.log(val.length);
-        console.log(fullArr);
         setHeros(val.length === 0?fullArr:pos);
     }
     
-    console.log(heros);
-    if((!heros || heros.length === 0) && (!fullArr || fullArr.length === 0)){
-        return <Skeleton/>
-    }
   return (
-      <>
+    <div className="wrap">
         <Row Style="margin-top: 50px;">
             <Col span={24}>
                 <div className="searchHeros">
                     <Input onChange={searchHero} type="text" placeholder="Search"/>
                 </div>
             </Col>
-        </Row>  
-        <Row className="heroWrap">
+        </Row>
+        {(!heros || heros.length === 0) && <Skeleton/>}
+        {(heros && heros.length !== 0) && <Row className="heroWrap">
         {heros.map(hero=>
             <Col key={hero.id} xl={4} xs={24} lg={24} sm={24} md={6}>
                 <div className="heroBox">
@@ -56,8 +45,8 @@ const FetchHeros = function(){
                 </div>
             </Col>
         )}
-        </Row>
-      </>
+        </Row>}
+    </div>
       
   )
 }
--- task/services.js
+++ task/services.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getMarvelCharactars = (value) => {
     return axios({
       method: "get",
-      url: "https://gateway.marvel.com/v1/public/characters?apikey=6cc8993f6724fc4b9830a756970bcbeb",
+      url: "https://gateway.marvel.com/v1/public/characters?limit=30&apikey=6cc8993f6724fc4b9830a756970bcbeb",
     }).catch((error) => {
       console.log(error);
     });
